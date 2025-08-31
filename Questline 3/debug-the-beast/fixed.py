6cc41756695a51e85828f4b4d7a1001b81a864f0
bhavyaanil07 <anilsumi358@gmail.com>
2025-08-31 16:47:06 +0530

    Add files via upload

---
+++ Questline 3/debug-the-beast/fixed.py
@@ -0,0 +1,15 @@
+#Program: Find the second largest number in a list
+def second_largest(arr):
+    largest = second = -9999999
+    for num in arr:
+        if num > largest:
+            second = largest
+            largest = num
+        elif num > second and num!= largest:
+            second = num
+    if second==-9999999:
+        second = 'none'
+    return second
+n = int(input("Enter number of elements: "))
+arr = list(map(int, input("Enter numbers: ").split()))
+print("Second largest:", second_largest(arr))
